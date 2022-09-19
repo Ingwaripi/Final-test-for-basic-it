2110aedc79e9b3e4f031003e383a9e858dd9dca5
Ekaterina <ekaterina266226@mail.ru>
2022-09-19 15:11:40 +0300

    add array creation and validationmassive

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,25 @@
-﻿string[] Array;
+﻿string[] arr1 = { "Hello", "2", "world", ":-)" };
+string[] arr2 = { "1234", "1567", "-2", "computer science" };
+string[] arr3 = { "Russia", "Denmark", "Kazan" };
 
-Console.WriteLine("Введите массив строк через пробел");
-string stroky = Console.ReadLine();
-Array = stroky.Split("");
+int maxLehght = 3;
+
+string[] GetShortWordsArray(string[] array)
+{
+    int length = array.Length;
+    string[] result = new string[length];
+    int count = 0;
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i].Length <= maxLehght)
+        {
+            result[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref result, count);
+
+    return result;
+}
+
